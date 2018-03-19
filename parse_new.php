a8bd8e65515e1583373541bb1769a12e71584571
sahil gupta <sahil7gupta@gmail.com>
2018-03-19 19:49:55 +0530

    modified parse_new

--- parse_new.php
+++ parse_new.php
@@ -2,7 +2,7 @@
 
 require_once('mysqli_connect.php');
 
-$textCnt  = "data.txt";
+$textCnt  = "articles.txt";
 $contents = file_get_contents($textCnt);
 rtrim($contents,'}');
 $arrfields = explode('}', $contents);
@@ -19,7 +19,7 @@ foreach($arrfields as $field) {
 	$arrfields2[2] = mysqli_real_escape_string($dbc, $arrfields2[2]);
 	$arrfields2[3] = mysqli_real_escape_string($dbc, $arrfields2[3]);
 	$arrfields2[4] = mysqli_real_escape_string($dbc, $arrfields2[4]);
-	$query = "INSERT INTO `news` (`NewsID`, `Title`, `date`, `URL`, `summary`) 
+	$query = "INSERT INTO `News` (`NewsID`, `Title`, `date`, `URL`, `summary`) 
 	VALUES ('$i', '$arrfields2[1]', '$arrfields2[2]', '$arrfields2[0]', '$arrfields2[3]');";
 	$response = @mysqli_query($dbc, $query);
 	$i++;
